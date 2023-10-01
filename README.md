78a575f58294115b72d9fc01e1e4b8d43ddf6022
Beyza Mercan <beyzamercanse@gmail.com>
2023-10-01 18:06:27 +0300

    commit all

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# Finance AI Chatbot NLTK 
- 
+# Finance AI Chatbot Project using PYTHON / NLTK
+
+## Overview
+
+Welcome to the Finance Chatbot project, guided by **Beyza Mercan**. This project is designed to equip you with the knowledge and skills to build a chatbot from scratch using Python. The chatbot, specifically tailored for finance-related inquiries, serves as a valuable tool for engaging with users and providing information on financial topics.
+
+## Project Description
+
+### Objective
+
+The primary goal of this project is to guide you through the process of building a chatbot capable of handling finance-related queries. Whether users seek information about stock prices, investment strategies, or general financial advice, the chatbot will provide relevant and insightful responses.
+
+### Features
+
+- **Natural Language Processing (NLP):** Implement NLP techniques to understand and interpret user queries effectively.
+
+- **Finance Data Integration:** Integrate finance data into the chatbot to provide real-time information on stock prices, market trends, and other financial indicators.
+
+- **User Interaction:** Develop an interactive and user-friendly interface to facilitate seamless communication between users and the chatbot.
+
+## Getting Started
+
+### Prerequisites
+
+- Python 3.x
+- Required dependencies (specified in `requirements.txt`)
+- Finance data API key (if applicable)
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/BeyzaMercan/finance-chatbot.git
+   ```
+
+1. Install dependencies:
+   ```bash
+   pip install -r requirements.txt
+   ```
+1. Set up your finance data API key (if needed).
+1. Run the chatbot script:
+   ```bash
+   python chatbot.py
+   ```
+
+### Usage
+
+Interact with the chatbot by entering finance-related queries.
+Explore the chatbot's responses, which are designed to provide informative and relevant information on finance topics.
+Customize the chatbot further to suit your specific finance-related use cases.
+Contributing
+
+Contributions are welcome! If you have ideas for enhancing the chatbot or want to address issues, please open an issue or submit a pull request.
+
+### License
+
+This project is licensed under the MIT License.
+
+### Acknowledgments
+
+This project is guided by Beyza Mercan.
+Developed as part of the ProjectPro platform.
+Special thanks to the open-source community for their valuable contributions.
+Feel free to customize this README file based on your preferences and provide additional details about the finance data used or specific functionalities of your chatbot.
---
+++ chatbot.py
@@ -0,0 +1,95 @@
+#Meet FinanceBot: Your Financial Assistant
+
+# import necessary libraries
+import io
+import random
+import string  # to process standard python strings
+import warnings
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+
+# Uncomment the following only the first time
+# nltk.download('punkt')  # first-time use only
+# nltk.download('wordnet')  # first-time use only
+
+from nltk.stem import WordNetLemmatizer
+import nltk
+
+# Download popular nltk packages
+nltk.download('popular', quiet=True)
+
+# Reading in the finance corpus
+with open('finance_corpus.txt', 'r', encoding='utf8', errors='ignore') as fin:
+    raw = fin.read().lower()
+
+# Tokenization
+sent_tokens = nltk.sent_tokenize(raw)  # converts to a list of sentences
+word_tokens = nltk.word_tokenize(raw)  # converts to a list of words
+
+# Preprocessing
+lemmer = WordNetLemmatizer()
+
+
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+
+
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
+
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+# Keyword Matching
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey",)
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+
+
+def greeting(sentence):
+    """If the user's input is a greeting, return a greeting response"""
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+
+# Generating response
+def response(user_response):
+    finance_response = ''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx = vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if req_tfidf == 0:
+        finance_response = finance_response + "I am sorry! I don't really understand you"
+        return finance_response
+    else:
+        finance_response = finance_response + sent_tokens[idx]
+        return finance_response
+
+
+flag = True
+print("FinanceBot: Hello! I'm FinanceBot, your financial assistant. I can help you with questions about finance. If you want to exit, type 'BYE.'")
+while flag:
+    user_response = input()
+    user_response = user_response.lower()
+    if user_response != 'bye':
+        if user_response == 'thanks' or user_response == 'thank you!':
+            flag = False
+            print("FinanceBot: You are more than welcome!")
+        else:
+            if greeting(user_response) is not None:
+                print("FinanceBot: " + greeting(user_response))
+            else:
+                print("FinanceBot: ", end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag = False
+        print("FinanceBot: Goodbye! Hope to assist you again.")
